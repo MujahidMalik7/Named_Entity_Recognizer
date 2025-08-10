06671bb84b89959bfb3bb7bcdefdeaea89da6ed0
Mujahid <mujahidtufail726@gmail.com>
2025-08-10 21:10:08 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+echo "Running setup.sh: downloading spaCy transformer model..."
+
 python -m spacy download en_core_web_trf
 
+if [ $? -eq 0 ]; then
+  echo "Model downloaded successfully."
+else
+  echo "Failed to download the model."
+  exit 1
+fi
